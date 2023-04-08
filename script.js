8a7ac0ac93f031f7ae1ba2837cea5ba6428657c9
4a <75583999+pixelamp1@users.noreply.github.com>
2023-04-07 19:14:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function newflvtext() {
-  const flvtext = ['bad video editor and even worse programmer', 'insert funny', 'aight', 'bad video editor and even worse programmer', '32°40’34.19″N 117° 9’27.58″W', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad blah blah blah shut up me', 'if ur reading this helo'];
-  let randomflvtext = Math.floor(Math.random() * flvtext.length);
+  var flvtext = ['bad video editor and even worse programmer', 'insert funny', 'aight', 'bad video editor and even worse programmer', '32°40’34.19″N 117° 9’27.58″W', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad video editor and even worse programmer', 'bad blah blah blah shut up me', 'if ur reading this helo'];
+  var randomflvtext = Math.floor(Math.random() * flvtext.length);
   document.getElementById('randomflvtext').innerHTML = flvtext[randomflvtext];
 })();
